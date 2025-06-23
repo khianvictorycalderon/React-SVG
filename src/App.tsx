020054887dcb87e736238e32e5fc7849adc312c3
khian calderon <ckhianvictory@gmail.com>
2025-06-23 20:48:42 +0800

    Added Circle

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,20 @@ function App() {
         React with SVG
       </h1>
       <hr/>
+      Circle:
+      <svg
+        width="100"
+        height="100"
+        viewBox="0 0 200 200"
+      >
+        <circle 
+          cx="100"
+          cy="100" 
+          r="50" 
+          fill="red" 
+          className="circle"
+          />
+      </svg>  
     </>
   )
 }
