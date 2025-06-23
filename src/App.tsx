615d206f3592cac66f3c1854f9e2574b4d416ec0
khian calderon <ckhianvictory@gmail.com>
2025-06-23 20:55:14 +0800

    Contained in Content

--- src/App.tsx
+++ src/App.tsx
@@ -10,29 +10,31 @@ function App() {
         React with SVG
       </h1>
       <hr/>
-      Circle Radius: {circleRadius}
-      <input
-        type="range"
-        min="10"
-        max="50"
-        value={circleRadius}
-        onChange={(e) => setCircleRadius(e.target.value)}
-        className="w-full"
-      />
-      <br/>
-      <svg
-        width="100"
-        height="100"
-        viewBox="0 0 200 200"
-      >
-        <circle 
-          cx="100"
-          cy="100" 
-          r={circleRadius} 
-          fill="red" 
-          className="circle"
-          />
-      </svg>  
+      <div className="content">
+        Circle Radius: {circleRadius}
+        <br/>
+        <input
+          type="range"
+          min="10"
+          max="50"
+          value={circleRadius}
+          onChange={(e) => setCircleRadius(e.target.value)}
+        />
+        <br/>
+        <svg
+          width="100"
+          height="100"
+          viewBox="0 0 200 200"
+        >
+          <circle 
+            cx="100"
+            cy="100" 
+            r={circleRadius} 
+            fill="red" 
+            className="circle"
+            />
+        </svg>  
+      </div>
     </>
   )
 }
